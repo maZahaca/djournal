f4f1180bc6db62a32b1086b63cf2731c90ee5c4f
Andrew Red <andrew@reddikh.com>
2023-04-09 00:28:57 +0100

    - added cycles of questions - added payment info

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
-import { Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
 import { Configuration, OpenAIApi } from 'openai';
-import { diaryAIMessageV2, diaryAIMessageV3, helloMessage, systemContext } from './templates';
+import {
+  badAIResponse,
+  diaryAIMessageV2,
+  diaryAIMessageV3,
+  diaryAISummary, diaryAISummaryRecommend,
+  diaryUserSummary,
+  helloMessage,
+  notADiaryNote,
+  notARealLifeCase,
+  notEnoughContext, subscriptionHeader, subscriptionNotReady, subscriptionNotSubscribed, subscriptionSubscribed,
+  systemContext,
+  systemError,
+} from './templates';
+import { getSubscription } from "./subscriptions";
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
+const subscriptionCycles = 1;
 
 const {
   BOT_TOKEN,
@@ -22,14 +37,138 @@ const openai = new OpenAIApi(new Configuration({
 const bot = new Telegraf(BOT_TOKEN!);
 
 type ChatMessageType = { role: 'system' | 'user' | 'assistant', content: string };
+type ChatStateType = {
+  messages: ChatMessageType[],
+  actions: { [key: string]: number },
+  cycles: number,
+  subscription: {
+    type: string,
+    status: 'subscribed' | 'unsubscribed',
+  },
+};
 
-const conversationContexts: { [key: string]: ChatMessageType[] } = {};
+const states: { [key: string]: ChatStateType } = {};
+
+const initStateForChat = (chatId: number) => {
+  states[chatId] = {
+    actions: {},
+    messages: [],
+    cycles: 0,
+    subscription: {
+      ...states[chatId]?.subscription
+        ? states[chatId]?.subscription
+        : { type: getSubscription(), status: 'unsubscribed' },
+    },
+  }
+};
+
+const getAICompletion = async (ctx: any, messages: ChatMessageType[]): Promise<string> => {
+  console.log('messages', messages);
+  await ctx.sendChatAction('typing');
+  let completion = await openai.createChatCompletion({
+    model: OPENAI_MODEL!,
+    messages: messages,
+  });
+  return completion!.data!.choices[0].message!.content;
+}
+
+const getActionOptions = (text: string): string[] => {
+  return text.split("\n").reduce((acc, cur): string[] => {
+    const matched = cur.match(/^((\d+\s?\.)|(-\s?))(.+)$/);
+    if (matched && matched[4]) {
+      acc.push(matched[4].trim());
+    }
+    return acc;
+  }, [] as string[]);
+};
+
+const processForChat = async (ctx: any, chatId: number, message: string) => {
+  if (!states[chatId]) {
+    initStateForChat(chatId);
+  }
+
+  const payMatched = message.match(/^pay-(.+)$/)
+  console.log('payMatched', payMatched, message);
+  if (payMatched && payMatched[1]) {
+    if (payMatched[1] === subscriptionNotReady()) {
+      ctx.reply(subscriptionNotSubscribed());
+    } else {
+      ctx.reply(subscriptionSubscribed());
+    }
+    return;
+  }
+  if (states[chatId].cycles >= subscriptionCycles) {
+    const subscriptionPlan = states[chatId].subscription.type;
+
+    const keyboard = Markup.inlineKeyboard([
+      Markup.button.callback(subscriptionPlan, `pay-${subscriptionPlan}`),
+      Markup.button.callback(subscriptionNotReady(), `pay-${subscriptionNotReady()}`),
+    ]);
+    await ctx.reply(`${subscriptionHeader()}`, keyboard);
+    return;
+  }
+
+  const messages: ChatMessageType[] = [];
+  for (const message of states[chatId].messages) {
+    messages.push(message);
+  }
+
+  try {
+    let completionText: string;
+    // first message about long diary record
+    if (states[chatId].messages.length === 0) {
+      messages.push({ role: 'user', content: diaryAIMessageV3(message) });
+    } else {
+      messages.push({ role: 'user', content: diaryAISummaryRecommend(message)  });
+    }
+    completionText = await getAICompletion(ctx, messages);
+    if (
+      !completionText ||
+      completionText.match(new RegExp(`^${badAIResponse()}`))
+    ) {
+      ctx.reply(systemError());
+      return;
+    }
+    if ([notEnoughContext(), notADiaryNote(), notARealLifeCase()].includes(completionText)) {
+      ctx.reply(completionText);
+      return;
+    }
+
+    if (completionText) {
+      ctx.reply(completionText);
+      console.log('question completion', completionText);
+      messages.push({ role: 'assistant', content: completionText });
+    }
+
+    // Summary
+    await ctx.sendChatAction('typing');
+    messages.push({ role: 'user', content: diaryAISummary() });
+    completionText = await getAICompletion(ctx, messages);
+    if (
+      !completionText ||
+      [notEnoughContext(), notADiaryNote(), notARealLifeCase()].includes(completionText) ||
+      completionText.match(new RegExp(`^${badAIResponse()}`))
+    ) {
+      return;
+    }
+    console.log('summary completion', completionText);
+    messages.push({ role: 'assistant', content: completionText });
+    const options = getActionOptions(completionText);
+    console.log('options', options);
+    const keyboard = Markup.inlineKeyboard(
+      options.map((o, i) => Markup.button.callback((i + 1).toString(), o))
+    );
+    await ctx.reply(`${diaryUserSummary()}\n${completionText}`, keyboard);
+  } catch (error) {
+    console.error(error);
+  }
+  states[chatId].messages = messages;
+  states[chatId].cycles++;
+}
 
 bot.start(async (ctx) => {
   // reset conversations context for the chat when /start called
-  if (conversationContexts[ctx.message.chat.id]) {
-    delete conversationContexts[ctx.message.chat.id];
-  }
+  initStateForChat(ctx.message.chat.id);
   const messages = helloMessage();
   await messages.reduce(async (prev, message, index) => {
     await prev;
@@ -45,37 +184,12 @@ bot.start(async (ctx) => {
 
 bot.on(message('text'), async (ctx) => {
   const { id: chatId } = ctx.message.chat;
-  await ctx.sendChatAction('typing');
+  await processForChat(ctx, chatId, ctx.message.text);
+});
 
-  if (!conversationContexts[chatId]) {
-    conversationContexts[chatId] = [
-      // { role: 'system', content: systemContext() },
-    ];
-  }
-  const messages: ChatMessageType[] = [];
-  for (const message of conversationContexts[chatId]) {
-    messages.push(message);
-  }
-
-  messages.push({ role: 'user', content: diaryAIMessageV3(ctx.message.text) });
-  conversationContexts[chatId] = messages;
-
-  try {
-    console.log('messages', messages);
-    const completion = await openai.createChatCompletion({
-      model: OPENAI_MODEL!,
-      messages: messages,
-    });
-
-    const completionText = completion!.data!.choices[0].message!.content;
-    ctx.reply(completionText);
-    console.log('completionText', completionText);
-
-    messages.push({ role: 'assistant', content: completionText });
-    conversationContexts[chatId] = messages;
-  } catch (error) {
-    console.error(error);
-  }
+bot.action(/.*/, async (ctx) => {
+  const { id: chatId } = ctx.update.callback_query.message!.chat;
+  await processForChat(ctx, chatId, ctx.match.input);
 });
 
 bot.help((ctx) => ctx.reply('Send me a sticker'));
